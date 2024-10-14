bc812cd4a1840447643ab193dedf577441525443
SiamakMehrnia <siamakme20@gmail.com>
2024-10-14 15:54:15 +0200

    lent

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,48 @@
-
-
-
 import { AppProvider } from "../context/AppContext.jsx";
 import Budget from "../components/Budget.jsx";
 import ExpenseTotal from "../components/ExpenseTotal.jsx";
 import ExpenseList from "../components/ExpenseList.jsx";
 import AddExpenseForm from "../components/AddExpenseForm";
 import RemainingBudget from "../components/Remaining.jsx";
+import LentMoney from "../components/LentMoney.jsx";
 
 const App = () => {
-	return (
-		<AppProvider>
-			<div className='container'>
-				<h1 className='mt-3'>My Budget Planner</h1>
-				<div className='row mt-3'>
-					<div className='col-sm'>
-						<Budget />
-					</div>
-					<div className='col-sm'>
-						<RemainingBudget />
-					</div>
-					<div className='col-sm'>
-						<ExpenseTotal />
-					</div>
-				</div>
-				<h3 className='mt-3'>Expenses</h3>
-				<div className='row '>
-					<div className='col-sm'>
-						<ExpenseList />
-					</div>
-				</div>
-				<h3 className='mt-3'>Add Expense</h3>
-				<div className='row mt-3'>
-					<div className='col-sm'>
-						<AddExpenseForm />
-					</div>
-				</div>
-			</div>
-		</AppProvider>
-	);
+  return (
+    <AppProvider>
+      <div className="container">
+        <h1 className="mt-3">My Budget Planner</h1>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <Budget />
+          </div>
+          <div className="col-sm">
+            <RemainingBudget />
+          </div>
+          <div className="col-sm">
+            <ExpenseTotal />
+          </div>
+        </div>
+        <h3 className="mt-3">Expenses</h3>
+        <div className="row ">
+          <div className="col-sm">
+            <ExpenseList />
+          </div>
+        </div>
+        <h3 className="mt-3">Expenses</h3>
+        <div className="row ">
+          <div className="col-sm">
+            <LentMoney />
+          </div>
+        </div>
+        <h3 className="mt-3">Add Expense</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <AddExpenseForm />
+          </div>
+        </div>
+      </div>
+    </AppProvider>
+  );
 };
 
 export default App;
